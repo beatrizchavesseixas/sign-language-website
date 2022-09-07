a64050614dc77593d0e946d3b93e8a38e53c6441
Beatriz Seixas <biaseixas15@hotmail.com>
2022-09-07 15:20:37 +0100

    ...

--- pages/Real_Case.py
+++ pages/Real_Case.py
@@ -1,13 +1,30 @@
 import streamlit as st
 from PIL import Image
 
-st.markdown("""
-
-    # MEDICAL SERVICES
-
-    """)
+#Display logo on the left and titleon the right
+col1, col2 = st.columns([1, 6])
+medical_image = Image.open('image.png')
+with col1:
+    st.image(medical_image, width=100)
+with col2:
+    #st.markdown('<h1 style="float: left;">MEDIAL SERVICES</h1><img style="float: right;" src="MEDICAL SYMBOL.PNG" />', unsafe_allow_html=True)
+    st.title("MEDICAL SERVICES")
 
 
 #Visualize patient info
 st.subheader("Patient details")
 st.text("Name: XXXXX XXXXX \nAge:XX\nGenre: XXXXX")
+
+
+#Open camera on the left and display symptoms on the right
+col1, col2 = st.columns([1, 1])
+with col1:
+    run = st.button("Translate patient's symptoms")
+    frame_window = st.image([])
+    cam = cv2.VideoCapture(0)
+    while run:
+        ret, frame = cam.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_window.image(frame)
+        #with col2:
+            #st.text("headache \ntired \nsore throat")
