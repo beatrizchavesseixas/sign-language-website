fe4da78a55a93591ef2c59a1fa15c6ad11617c2c
Beatriz Seixas <biaseixas15@hotmail.com>
2022-09-07 15:14:56 +0100

    ...

--- pages/Real_Case.py
+++ pages/Real_Case.py
@@ -2,13 +2,8 @@ import streamlit as st
 from PIL import Image
 
 #Display logo on the left and titleon the right
-col1, col2 = st.columns([1, 6])
-medical_image = Image.open('medical_symbol.png')
-with col1:
-    st.image(medical_image, width=100)
-with col2:
-    #st.markdown('<h1 style="float: left;">MEDIAL SERVICES</h1><img style="float: right;" src="MEDICAL SYMBOL.PNG" />', unsafe_allow_html=True)
-    st.title("MEDICAL SERVICES")
+
+
 
 #Visualize patient info
 st.subheader("Patient details")
