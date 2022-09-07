95b0e9b3c209fa5e93c496babdded766aaf2962b
Beatriz Seixas <biaseixas15@hotmail.com>
2022-09-07 16:39:25 +0100

    ...

--- pages/Real_Case.py
+++ pages/Real_Case.py
@@ -3,9 +3,6 @@ from PIL import Image
 
 #Display logo on the left and titleon the right
 col1, col2 = st.columns([1, 6])
-medical_image = Image.open('med_symbol.jpg')
-with col1:
-    st.image(medical_image, width=100)
 with col2:
     #st.markdown('<h1 style="float: left;">MEDIAL SERVICES</h1><img style="float: right;" src="MEDICAL SYMBOL.PNG" />', unsafe_allow_html=True)
     st.title("MEDICAL SERVICES")
