434751e107b6d1f4c024ad124c602f68769ce3b2
Beatriz Seixas <biaseixas15@hotmail.com>
2022-09-08 11:32:31 +0100

    ...

--- pages/Model.py
+++ pages/Model.py
@@ -14,5 +14,8 @@ st.subheader("Patient details")
 st.text("Name: Patricia Washington \nAge:31\nGenre: Female")
 
 
-biagif= Image.open('beatriz.gif')
-st.image(biagif, use_column_width=False)
+file = open(r"/home/beatrizclseixas/code/beatrizchavesseixas/sign-language-website/beatriz.gif", 'rb')
+contents = file.read()
+data_url = base64.b64encode(contents).decode('utf-8-sig')
+file.close()
+st.markdown(f'<img src="data:image/gif;base64,{data_url}>',unsafe_allow_html = True)
