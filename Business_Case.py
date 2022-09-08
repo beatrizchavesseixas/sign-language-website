48a0c35a56aae90fe46ee4c9b914f66719453808
Beatriz Seixas <biaseixas15@hotmail.com>
2022-09-08 11:52:13 +0100

    ...

--- Business_Case.py
+++ Business_Case.py
@@ -12,3 +12,7 @@ st.markdown("""
     #### *What about a sign language translator to improve communication between healthcare professionals and deaf patients?* :open_hands:
 
 """)
+
+
+biavideo = open("beatriz_novo.mp4", "rb")
+st.video(biavideo)
